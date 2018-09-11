4d12ca8e9318e46ab243870f69f774c6467bf933
zhong <384232149@qq.com>
2018-09-11 15:15:37 +0800

    test clipboard

---
+++ src/data/blog/items/6.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import ClipboardJS from 'clipboard'
+
+export default class Content extends Component {
+
+
+  componentDidMount () {
+    const clip1 = new ClipboardJS('.btn');
+    clip1.on('success', function () { alert('成功') });
+    clip1.on('error', function () { alert('失败') });
+
+    const clip3 = new ClipboardJS('#btn3', {
+      text: function() {
+        return document.getElementById('test3').value;
+      }
+    });
+    clip3.on('success', function () { alert('3-成功') });
+    clip3.on('error', function () { alert('3-失败') });
+
+    const clip4 = new ClipboardJS('#btn4', {
+      target: function() {
+        return document.getElementById('test4');
+      }
+    });
+    clip4.on('success', function () { alert('4-成功') })
+    clip4.on('error', function () { alert('4-失败') })
+  }
+
+  componentWillUnmount () {
+    
+  }
+
+  render () {
+    return (
+      <div>
+        <div className="test-group">
+          <h4>Test one</h4>
+          <div>
+            <span>测试内容：1111 </span><button className="btn" data-clipboard-text="1111">复制</button>
+          </div>
+        </div>
+
+        <div className="test-group">
+          <h4>Test two</h4>
+          <div>
+            <span>测试内容：</span>
+            <input id="test2" defaultValue="2222" />
+            <button className="btn" data-clipboard-target="#test2">复制</button>
+          </div>
+        </div>
+
+        <div className="test-group">
+          <h4>Test three</h4>
+          <div>
+            <span>测试内容：</span>
+            <input id="test3" defaultValue="3333" />
+            <button id="btn3">复制</button>
+          </div>
+        </div>
+
+        <div className="test-group">
+          <h4>Test four</h4>
+          <div>
+            <span>测试内容：</span>
+            <input id="test4" defaultValue="4444" />
+            <button id="btn4">复制</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+}
--- src/data/blog/list.js
+++ src/data/blog/list.js
@@ -8,4 +8,5 @@ export default [
   {id: 3, title: '原生js——单图/多图/图库（自定义api提供）图片选取 预览并上传', subtitle: '这里有一个副标题，内容是什么呢我还没想好。想想应该还有vue、react组件版', time: '2018-07-31', category: 'javascript'},
   {id: 4, title: '日常随手记录', time: '2018-08-27', category: 'other'},
   {id: 5, title: '将url的所有参数输出为一个对象', subtitle: '场景：我们需要根据页面url中的id等参数去请求接口api', time: '2018-08-27', category: 'javascript'},
+  {id: 6, title: 'Clipboard测试', subtitle: '', time: '2018-09-11', category: 'javascript'},
 ]
