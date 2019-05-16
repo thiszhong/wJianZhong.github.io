962d19184f08023a75e828e151683055a5d61fe9
zhong <384232149@qq.com>
2019-05-16 11:11:44 +0800

    reset

--- README.md
+++
@@ -1,3 +0,0 @@
-# thiszhong.github.io
-My site here, welcome to you!
-The content: [https://thiszhong.github.io/blog](https://thiszhong.github.io/blog "index")
--- src/data/blog/items/1.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Content extends Component {
-  render () {
-    return (
-      <div>Hello item1</div>
-    )
-  }
-}
